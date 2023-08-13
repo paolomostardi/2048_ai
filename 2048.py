492a96d0a9f1a95fac35d57f15b5f340add54245
paolo <paolomostardi@gmail.com>
2023-08-13 17:52:09 +0200

    fix: fixing Y and X wrong positioning

--- 2048.py
+++ 2048.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from logic2048 import Logic2048
 import math
+import copy
 
 """
 
@@ -9,6 +10,12 @@ import math
 4 5 6 7
 8 9 10 11
 12 13 14 15
+
+
+rect = pygame.Rect(i * 110 + 350, j * 110 + 50, 100, 100)
+
+
+
 
 """
 
@@ -18,20 +25,22 @@ import math
 class Board2048:
     def __init__(self, size, padding, screen):
 
-        self.grey = (224, 224, 224)
+        self.grey = [224, 224, 224]
+        self.orange = [255, 127, 80]
         self.screen = screen
 
         self.padding = padding
         self.square_list = []
-        self.orange = (255, 127, 80)
-        self.size = size/4
+        self.size = size//4
         self.logical_board = Logic2048()
 
     def render(self):
         for index, square in enumerate(self.logical_board.square_list):
+
             x, y = self.calculate_coordinates(index)
             color = self.get_color_from_number(square)
-            rectangle = pygame.Rect(self.size, self.size, x, y)
+
+            rectangle = pygame.Rect(x,y, self.size-5, self.size-5)
 
             pygame.draw.rect(self.screen, color, rectangle)
 
@@ -39,33 +48,40 @@ class Board2048:
         padding = self.padding
 
         if index == 0:
-            return 0, 0
+            return padding, padding
 
-        x = 4 % index
+        x = index % 4
         y = index // 4
-        x *= self.size + padding
-        y *= self.size + padding
+
+        x *= self.size
+        y *= self.size
+        x += padding
+        y += padding
 
         return x, y
 
     def on_move_down(self):
         self.logical_board.slide_down()
 
-
     def get_color_from_number(self,square):
         color = self.grey
 
         if square != 0:
-            color = math.log2(square)
 
-        return self.grey
+            r = 255
+            g = 255 - (255/11) * math.log2(square)
+            b = 0
+            color = (r, g, b)
+
+        return color
+
 
 def main():
 
-    WIDTH = 1200
+    WIDTH = 800
     HEIGHT = 800
-    board_size = 700
-    padding = 100
+    board_size = 750
+    padding = 25
 
     running = True
 
@@ -89,7 +105,7 @@ def main():
                     board.on_move_down()
                 elif event.key == pygame.K_UP:
                     print('hello')
-                board.generate_entry()
+
 
 
         screen.fill((200, 200, 200))
