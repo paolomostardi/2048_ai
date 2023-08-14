b851651c4f952c370999d8559de45fb78f14497b
paolo <paolomostardi@gmail.com>
2023-08-14 13:59:42 +0200

    Feature: board now display number on tile

--- 2048.py
+++ 2048.py
@@ -30,6 +30,7 @@ class Board2048:
         self.square_list = []
         self.size = size//4
         self.logical_board = Logic2048()
+        self.font = pygame.font.SysFont('Times new roman', 80)
 
     def render(self):
         for index, square in enumerate(self.logical_board.square_list):
@@ -39,7 +40,12 @@ class Board2048:
 
             rectangle = pygame.Rect(x, y, self.size-5, self.size-5)
 
+            text_surface = self.font.render(str(square), False, (255, 255, 255))
+
             pygame.draw.rect(self.screen, color, rectangle)
+
+            if square != 0:
+                self.screen.blit(text_surface, (x + 75, y + 50))
 
     def calculate_coordinates(self, index):
         padding = self.padding
@@ -91,6 +97,9 @@ def main():
 
     running = True
 
+    pygame.font.init()  # you have to call this at the start,
+    # if you want to use this module.
+
     clock = pygame.time.Clock()
     framerate = 15
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -121,6 +130,7 @@ def main():
         color = (50, 200, 200)
 
         board.render()
+
 
         pygame.display.update()
         clock.tick(framerate)
