5f789faad1b808b73bfaa74be89cfced4f7d629e
paolo <paolomostardi@gmail.com>
2023-08-15 12:11:30 +0200

    fix: fixed bug of board merging twice in a row

--- logic2048.py
+++ logic2048.py
@@ -38,20 +38,24 @@ class Logic2048:
         for index, square in enumerate(self.square_list):
             index = len(self.square_list) - index - 1
             if index - 3 == 0:
-                return
-            if self.square_list[index] == self.square_list[index - 4]:
+                return action_performed
+            if self.square_list[index] == 0:
+                continue
+            elif self.square_list[index] == self.square_list[index - 4]:
                 self.square_list[index - 4] = 0
                 self.square_list[index] *= 2
+                print(self.square_list[index])
+                print(self.square_list[index - 4])
                 action_performed = True
-
-        return action_performed
 
     def merge_up(self):
         action_performed = False
 
         for index, square in enumerate(self.square_list):
             if index < 3:
-                print()
+                continue
+            if self.square_list[index] == 0:
+                continue
             elif self.square_list[index] == self.square_list[index - 4]:
                 self.square_list[index - 4] = self.square_list[index] * 2
                 self.square_list[index] = 0
@@ -65,9 +69,9 @@ class Logic2048:
         for index, square in enumerate(self.square_list):
 
             if square == 0:
-                print('')
+                continue
             elif index % 4 == 0:
-                print('')
+                continue
 
             elif self.square_list[index] == self.square_list[index - 1]:
                 self.square_list[index - 1] = self.square_list[index] * 2
@@ -81,10 +85,12 @@ class Logic2048:
 
         for index, square in enumerate(self.square_list):
 
-            if square == 0:
-                print('')
+            index = len(self.square_list) - index - 1
+
+            if self.square_list[index] == 0:
+                continue
             elif index % 4 == 3:
-                print('')
+                continue
 
             elif self.square_list[index] == self.square_list[index + 1]:
                 self.square_list[index + 1] = self.square_list[index] * 2
@@ -100,13 +106,16 @@ class Logic2048:
             length_list = len(self.square_list)
             index = length_list - index - 1
 
-            while index + 4 < length_list:
-                if self.square_list[index + 4] == 0:
-                    self.square_list[index + 4] = self.square_list[index]
-                    self.square_list[index] = 0
-                    action_performed = True
+            if self.square_list[index] == 0:
+                continue
+            else:
+                while index + 4 < length_list:
+                    if self.square_list[index + 4] == 0:
+                        self.square_list[index + 4] = self.square_list[index]
+                        self.square_list[index] = 0
+                        action_performed = True
 
-                index += 4
+                    index += 4
 
         return action_performed
 
@@ -114,7 +123,9 @@ class Logic2048:
         action_performed = False
 
         for index, square in enumerate(self.square_list):
-            length_list = len(self.square_list)
+
+            if square == 0:
+                continue
 
             while index > 3:
                 if self.square_list[index - 4] == 0:
@@ -131,6 +142,9 @@ class Logic2048:
 
         for index, square in enumerate(self.square_list):
 
+            if square == 0:
+                continue
+
             while index % 4 != 0:
                 if self.square_list[index - 1] == 0:
                     self.square_list[index - 1] = self.square_list[index]
@@ -145,6 +159,8 @@ class Logic2048:
         action_performed = False
         for index, square in enumerate(self.square_list):
 
+            if square == 0:
+                continue
             while index % 4 != 3:
                 if self.square_list[index + 1] == 0:
                     self.square_list[index + 1] = self.square_list[index]
@@ -155,34 +171,39 @@ class Logic2048:
         return action_performed
 
     def on_down(self):
-        self.move_all_tiles_down()
-        self.merge_down()
-        self.move_all_tiles_down()
+        action_performed = self.move_all_tiles_down()
+        action_performed2 = self.merge_down()
+        action_performed3 = self.move_all_tiles_down()
 
-        self.generate_entry()
+        if action_performed or action_performed2 or action_performed3:
+            self.generate_entry()
 
     def on_left(self):
-        self.move_all_tiles_left()
-        self.merge_left()
-        self.move_all_tiles_left()
+        action_performed = self.move_all_tiles_left()
+        action_performed2 = self.merge_left()
+        action_performed3 = self.move_all_tiles_left()
 
-        self.generate_entry()
+        if action_performed or action_performed2 or action_performed3:
+            self.generate_entry()
         return
 
     def on_right(self):
-        self.move_all_tiles_right()
-        self.merge_right()
-        self.move_all_tiles_right()
+        action_performed = self.move_all_tiles_right()
+        action_performed2 = self.merge_right()
+        action_performed3 = self.move_all_tiles_right()
 
-        self.generate_entry()
+        if action_performed or action_performed2 or action_performed3:
+            self.generate_entry()
+
         return
 
     def on_up(self):
-        self.move_all_tiles_up()
-        self.merge_up()
-        self.move_all_tiles_up()
+        action_performed = self.move_all_tiles_up()
+        action_performed2 = self.merge_up()
+        action_performed3 = self.move_all_tiles_up()
 
-        self.generate_entry()
+        if action_performed or action_performed2 or action_performed3:
+            self.generate_entry()
         return
 
     def game_finish(self):
